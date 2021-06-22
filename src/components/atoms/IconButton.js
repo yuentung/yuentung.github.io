da3d612a1926f9ee07bee49f7027ba7c66aec15b
yuentung <jerryio1986@gmail.com>
2021-06-22 22:21:19 +0800

    feat: add IconButton component

---
+++ src/components/atoms/IconButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    width: ${({ sideLength }) => sideLength};
+    height: ${({ sideLength }) => sideLength};
+    padding: 0;
+    border-radius: 50%;
+    font-size: ${({ fontSize }) => fontSize};
+    line-height: ${({ sideLength }) => sideLength};
+    color: ${({ color }) => color};
+    background-color: ${({ theme }) => theme.background.avatarCard};
+    box-shadow: ${({ theme }) => theme.boxShadow};
+`;
+
+const IconButton = ({ icon, sideLength, fontSize, color, handleButtonClick }) => {
+    return (
+        <Button
+            className="material-icons-round"
+            sideLength={sideLength}
+            fontSize={fontSize}
+            color={color}
+            onClick={handleButtonClick}
+        >
+            {icon}
+        </Button>
+    );
+};
+
+export default IconButton;
