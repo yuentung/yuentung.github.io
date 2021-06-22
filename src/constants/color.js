6f81826d392890a7d8073526430aa6b95e0ecfc2
yuentung <jerryio1986@gmail.com>
2021-06-22 22:17:52 +0800

    feat: set colors

---
+++ src/constants/color.js
@@ -0,0 +1,44 @@
+const color = {
+    dark: {
+        text: {
+            primary: '#F2F2F0',
+            secondary: '#E5E5E5',
+            tertiary: '#A9A9A9',
+            keyword: ['#69E3D6', '#E9CC44'],
+        },
+        background: {
+            primary: '#1C1C1E',
+            secondary: '#2C2C2E',
+            tertiary: '#5B5B5D',
+            navbar: '#242424',
+            avatarCard: '#2C2C2E',
+        },
+        boxShadow: {
+            primary: '0 0 2px rgba(0, 0, 0, 0.3)',
+            navbar: '0 1px 2px rgba(0, 0, 0, 0.8)',
+            avatarCard: '0 0 2px rgba(0, 0, 0, 0.1)',
+        },
+    },
+    light: {
+        text: {
+            primary: '#1C1C1E',
+            secondary: '#2C2C2E',
+            tertiary: '#5B5B5D',
+            keyword: ['#3660DD', '#EE7A69'],
+        },
+        background: {
+            primary: '#F2F2F0',
+            secondary: '#E5E5E5',
+            tertiary: '#A9A9A9',
+            navbar: '#EEEEEE',
+            avatarCard: '#FFFFFF',
+        },
+        boxShadow: {
+            primary: '0 0 4px rgba(210, 210, 210, 0.5)',
+            navbar: '0 1px 2px rgba(0, 0, 0, 0.25)',
+            avatarCard: '0 0 2px rgba(210, 210, 210, 0.2)',
+        },
+    },
+};
+
+export default color;
