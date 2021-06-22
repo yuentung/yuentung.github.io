be741bab94d2d362ab0540a6822ea0d545c7abc3
yuentung <jerryio1986@gmail.com>
2021-06-22 22:19:32 +0800

    feat: set media query

---
+++ src/constants/media.js
@@ -0,0 +1,4 @@
+export const pc = '@media (max-width: 1040px)';
+export const pad = '@media (max-width: 834px)';
+export const largeMobile = '@media (max-width: 700px)';
+export const mobile = '@media (max-width: 414px)';
