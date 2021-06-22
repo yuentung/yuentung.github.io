ed707010a1ebbcb00e466ec2ff2f24b0eb891fef
yuentung <jerryio1986@gmail.com>
2021-06-22 22:22:17 +0800

    feat: add NavList component

---
+++ src/components/molecules/NavList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import useMedia from 'use-media';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import NavItem from '../atoms/NavItem';
+import { pad } from '../../constants/media';
+import data from '../../constants/data';
+
+const Wrapper = styled.ul`
+    display: flex;
+
+    ${pad} {
+        ${({ isSmallHeight }) => !isSmallHeight && `
+            flex-direction: column;
+            align-items:center;
+            width: 100%;
+        `}
+    }
+`;
+
+const NavList = ({ lang, sectionList, setIsOpen }) => {
+    const isPad = useMedia({ maxWidth: '834px' });
+    const isSmallHeight = useMedia({ maxHeight: '414px' });
+    const navbarHeight = useMemo(() => isPad ? 72 : 82, [isPad]);
+    const [selectedSection, setSelectedSection] = useState(document.elementFromPoint(0, navbarHeight).id || sectionList[0]);
+
+    useEffect(() => {
+        document.addEventListener('scroll', () => {
+            const currentSection = document.elementFromPoint(200, navbarHeight).id;
+            if (currentSection) {
+                setSelectedSection(currentSection);
+            }
+        });
+    }, [navbarHeight]);
+
+    return (
+        <Wrapper onClick={() => setIsOpen(false)} isSmallHeight={isSmallHeight}>
+            {sectionList.map(section => (
+                <NavItem
+                    key={section}
+                    id={section}
+                    isSelected={section === selectedSection}
+                    setSelectedSection={setSelectedSection}
+                >
+                    {data[lang].navbar[section]}
+                </NavItem>
+            ))}
+        </Wrapper>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        lang: state.lang,
+    };
+};
+
+export default connect(mapStateToProps)(NavList);
